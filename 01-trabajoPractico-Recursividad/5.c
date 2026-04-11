4bd41a585fac2d3678998421ba9e06d67328d772
NicolasCoco17 <125579210+NicolasCoco17@users.noreply.github.com>
2026-04-10 23:07:24 -0300

    Enhance number formatting with user input validation
    Added input validation for numeric strings and modified the main function to prompt for user input.

--- 01-trabajoPractico-Recursividad/5.c
+++ 01-trabajoPractico-Recursividad/5.c
@@ -2,6 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*5.	Generar un algoritmo recursivo que le ponga los “.” de los miles a un String de números.*/
+
+// Valida si el string contiene solo números
+int esNumero(char str[]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void agregarSeparadorMiles(char str[]) {
     int len = strlen(str);
 
@@ -30,9 +42,20 @@ void agregarSeparadorMiles(char str[]) {
 }
 
 void ejecutar_TP1_EJ05(){
-    char numero[] = "12345678";
+    char numero[100];
+
+    printf("Ingrese un numero: ");
+    scanf("%s", numero);
+
+    // Validación
+    while (!esNumero(numero)) {
+        printf("Error. Ingrese solo numeros: ");
+        scanf("%s", numero);
+    }
 
     agregarSeparadorMiles(numero);
+    printf("\n");
+
     system("pause");
     //return 0;
 }
