aa441d99f9137ab4fe2b91c6524e88ec0e9249bd
esteft660-ai <esteft660@gmail.com>
2026-04-30 13:31:08 -0600

    Update main.h

--- 02-trabajoPractico-Listas/main.h
+++ 02-trabajoPractico-Listas/main.h
@@ -2,16 +2,11 @@
 #ifndef MAIN_TP1_H
 #define MAIN_TP1_H
 
-//void ejecutar_TP1_EJ01();
+
 void ejecutar_TP2_EJ02();
 void ejecutar_TP2_EJ03();
 void ejecutar_TP2_EJ04();
 void ejecutar_TP2_EJ05();
 void ejecutar_TP2_EJ06();
-//void ejecutar_TP1_EJ07();
-//void ejecutar_TP1_EJ08();
-//void ejecutar_TP1_EJ09();
-//void ejecutar_TP1_EJ10();
-
 
 #endif
