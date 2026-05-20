93854a813a2a7031b1e6e6c1935af07c63d3d31f
esteft660-ai <esteft660@gmail.com>
2026-05-20 13:38:51 -0600

    Update ejerciciosTP3.c

--- 03-trabajoPractico-Pilas/ejerciciosTP3.c
+++ 03-trabajoPractico-Pilas/ejerciciosTP3.c
@@ -40,38 +40,37 @@ void ejecutar_TP3_EJ06() {
     printf("Ejercicio 6 \n");
 
     Pila p = p_crear();
-
-int cantidad;
-int valor;
-int eliminar;
-
-printf("Ingrese cantidad de elementos: ");
-scanf("%d", &cantidad);
-
-for (int i = 0; i < cantidad; i++) {
-
-    printf("Ingrese valor: ");
-    scanf("%d", &valor);
-
-    p_apilar(p, te_crear(valor));
-}
-
-printf("\nPila original:\n");
-
-p_mostrar(p);
-
-printf("\nIngrese clave a eliminar: ");
-scanf("%d", &eliminar);
-
-Pila nueva = p_ej6_eliminarclave(p, eliminar);
-
-printf("\nNueva pila:\n");
-
-p_mostrar(nueva);
-
-printf("\nPila original conservada:\n");
-
-p_mostrar(p);
+    
+    int cantidad;
+    int valor;
+    int eliminar;
+    
+    printf("Ingrese cantidad de elementos: ");
+    scanf("%d", &cantidad);
+    
+    for (int i = 0; i < cantidad; i++) {
+        printf("Ingrese valor: ");
+        scanf("%d", &valor);
+        
+        p_apilar(p, te_crear(valor));
+    }
+    
+    printf("\nPila original:\n");
+    
+    p_mostrar(p);
+    
+    printf("\nIngrese clave a eliminar: ");
+    scanf("%d", &eliminar);
+    
+    Pila nueva = p_ej6_eliminarclave(p, eliminar);
+    
+    printf("\nNueva pila:\n");
+    
+    p_mostrar(nueva);
+    
+    printf("\nPila original conservada:\n");
+    
+    p_mostrar(p);
 
 }
 
@@ -80,106 +79,105 @@ void ejecutar_TP3_EJ07() {
 
     printf("Ejercicio 7 \n");
     
-Pila p1 = p_crear(); 
-
-Pila p2 = p_crear(); 
-
-int cantidad1; 
-int cantidad2; 
-int valor; 
-
-printf("Cantidad elementos pila 1: "); 
-scanf("%d", &cantidad1); 
+    Pila p1 = p_crear(); 
     
-for (int i = 0; i < cantidad1; i++) { 
-    printf("Ingrese valor: "); 
-    scanf("%d", &valor); 
-    p_apilar(p1, te_crear(valor)); 
-} 
-
-printf("\nCantidad elementos pila 2: "); 
-scanf("%d", &cantidad2); 
+    Pila p2 = p_crear(); 
     
-for (int i = 0; i < cantidad2; i++) { 
-    printf("Ingrese valor: "); 
-    scanf("%d", &valor); 
-    p_apilar(p2, te_crear(valor)); 
-} 
-
-printf("\nPila 1:\n"); 
-
-p_mostrar(p1); 
+    int cantidad1; 
+    int cantidad2; 
+    int valor; 
     
-printf("\nPila 2:\n"); 
+    printf("Cantidad elementos pila 1: "); 
+    scanf("%d", &cantidad1); 
     
-p_mostrar(p2); 
+    for (int i = 0; i < cantidad1; i++) { 
+        printf("Ingrese valor: "); 
+        scanf("%d", &valor); 
+        
+        p_apilar(p1, te_crear(valor)); 
+    } 
     
-Pila comunes = p_ej7_elementoscomunes(p1, p2); 
-
-printf("\nElementos comunes:\n"); 
+    printf("\nCantidad elementos pila 2: "); 
+    scanf("%d", &cantidad2); 
     
-p_mostrar(comunes); 
+    for (int i = 0; i < cantidad2; i++) { 
+        printf("Ingrese valor: "); 
+        scanf("%d", &valor); 
+        p_apilar(p2, te_crear(valor)); 
+    } 
     
-printf("\nPilas originales conservadas:\n"); 
-
-printf("\nPila 1:\n"); 
+    printf("\nPila 1:\n"); 
     
-p_mostrar(p1); 
+    p_mostrar(p1); 
     
-printf("\nPila 2:\n"); 
+    printf("\nPila 2:\n"); 
     
-p_mostrar(p2);
+    p_mostrar(p2); 
+    
+    Pila comunes = p_ej7_elementoscomunes(p1, p2); 
+    
+    printf("\nElementos comunes:\n"); 
+    
+    p_mostrar(comunes); 
+    
+    printf("\nPilas originales conservadas:\n"); 
+    
+    printf("\nPila 1:\n"); 
+    
+    p_mostrar(p1); 
+    
+    printf("\nPila 2:\n"); 
+    
+    p_mostrar(p2);
 }
 
 void ejecutar_TP3_EJ08() {
     
 
     printf("Ejercicio 8 \n");
-Pila p = p_crear();
-
-int cantidad;
-int valor;
-
-printf("Cantidad de elementos: ");
-scanf("%d", &cantidad);
-
-for (int i = 0; i < cantidad; i++) {
-
-    printf("Ingrese valor: ");
-    scanf("%d", &valor);
-
-    p_apilar(p, te_crear(valor));
-}
-
-printf("\nPila original:\n");
-
-p_mostrar(p);
-
-Pila resultado = p_ej8_sacarrepetidos(p);
-
-printf("\nValores unicos y cantidades:\n");
-
-Pila aux = p_crear();
-
-while (!p_es_vacia(resultado)) {
-
-    TipoElemento x = p_desapilar(resultado);
-
-    int cantidad = *((int*)x->valor);
-
-    printf("%d : %d\n", x->clave, cantidad);
-
-    p_apilar(aux, x);
-}
-
-while (!p_es_vacia(aux)) {
-
-    p_apilar(resultado, p_desapilar(aux));
-}
-
-printf("\nPila original conservada:\n");
-
-p_mostrar(p);
+    
+    Pila p = p_crear();
+    
+    int cantidad;
+    int valor;
+    
+    printf("Cantidad de elementos: ");
+    scanf("%d", &cantidad);
+    
+    for (int i = 0; i < cantidad; i++) {
+        printf("Ingrese valor: ");
+        scanf("%d", &valor);
+        
+        p_apilar(p, te_crear(valor));
+    }
+    
+    printf("\nPila original:\n");
+    
+    p_mostrar(p);
+    
+    Pila resultado = p_ej8_sacarrepetidos(p);
+    
+    printf("\nValores unicos y cantidades:\n");
+    
+    Pila aux = p_crear();
+    
+    while (!p_es_vacia(resultado)) {
+        TipoElemento x = p_desapilar(resultado);
+        
+        int cantidad = *((int*)x->valor);
+        
+        printf("%d : %d\n", x->clave, cantidad);
+        
+        p_apilar(aux, x);
+    }
+    
+    while (!p_es_vacia(aux)) {
+        p_apilar(resultado, p_desapilar(aux));
+    }
+    
+    printf("\nPila original conservada:\n");
+    
+    p_mostrar(p);
 
 }
 
