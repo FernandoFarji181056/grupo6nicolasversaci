d2d871b4612f7ce400b6a5eee1b7d2dbf1b0a995
esteft660-ai <esteft660@gmail.com>
2026-05-25 17:08:46 -0600

    Update ejerciciosTP4.c

--- 04-trabajoPractico-Colas/ejerciciosTP4.c
+++ 04-trabajoPractico-Colas/ejerciciosTP4.c
@@ -1,1 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../libs/tipoElemento/headers/tipo_elemento.h"
+#include "../libs/validaciones/headers/Validacion_datos.h"
+#include "../libs/colas/headers/colas.h"
+#include "tp_4_colas.h"
+#include "../libs/validaciones/headers/validaciones.h"
 
+/*  Esta función pertenece al ejercicio 2
+    del TP de colas 
+*/
+
+void ejecutar_TP4_EJ02() {
+
+    clearScreen();
+
+    printf("============================================\n");
+    printf("           Elegiste el ejercicio 2\n");
+    printf("============================================\n");
+
+
+    printf("\n=====================================\n");
+    printf("         FIN DEL EJERCICIO 2\n");
+    printf("=====================================\n");
+
+    return;
+
+
+}
+
+/*  Esta función pertenece al ejercicio 3
+    del TP de colas
+*/
+void ejecutar_TP4_EJ03() {
+
+    clearScreen();
+
+
+    printf("================================================\n");
+    printf("           Elegiste el ejercicio 3\n");
+    printf("================================================\n");
+
+
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 3\n");
+    printf("=====================================\n");
+
+    return;
+
+}
+
+/*  Esta función pertenece al ejercicio 4
+    del TP de colas 
+*/
+void ejecutar_TP3_EJ04() {
+
+    clearScreen();
+
+    printf("========================================================\n");
+    printf("           Elegiste el ejercicio 4\n");
+    printf("======================================================\n");
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 4\n");
+    printf("=====================================\n");
+
+
+    return;
+
+}
+
+
+/*  Esta función pertenece al ejercicio 5
+    del TP de colas
+*/
+void ejecutar_TP3_EJ05() {
+
+    clearScreen();
+
+    printf("========================================================\n");
+    printf("           Elegiste el ejercicio 5\n");
+    printf("======================================================\n");
+
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 5\n");
+    printf("=====================================\n");
+
+    return;
+
+}
+
+
+void ejecutar_TP3_EJ06() {
+    
+
+    printf("Ejercicio 6 \n");
+
+    
+
+    printf("=====================================\n");
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 5\n");
+    printf("=====================================\n");
+
+    return;
+}
+
+void ejecutar_TP4_EJ07() {
+    
+
+    printf("Ejercicio 7 \n");
+
+
+    printf("=====================================\n");
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 7\n");
+    printf("=====================================\n");
+
+    return;
+}
+
+void ejecutar_TP4_EJ08() {
+    
+
+    printf("Ejercicio 8 \n");
+    
+    
+    
+    printf("=====================================\n");
+
+    printf("\n=====================================\n");
+    printf("        FIN DEL EJERCICIO 8\n");
+    printf("=====================================\n");
+
+    return;
+
+}
