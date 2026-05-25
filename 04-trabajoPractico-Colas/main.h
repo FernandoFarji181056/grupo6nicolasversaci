9ad8f8c02486b8f1733e4dd84e08366a310e4479
esteft660-ai <esteft660@gmail.com>
2026-05-25 15:05:04 -0600

    Update main.h

--- 04-trabajoPractico-Colas/main.h
+++ 04-trabajoPractico-Colas/main.h
@@ -1,1 +1,13 @@
 
+#ifndef MAIN_TP4_H
+#define MAIN_TP4_H
+
+
+void ejecutar_TP4_EJ02();
+void ejecutar_TP4_EJ03();
+void ejecutar_TP4_EJ04();
+void ejecutar_TP4_EJ05();
+void ejecutar_TP4_EJ06();
+void ejecutar_TP4_EJ07();
+
+#endif
