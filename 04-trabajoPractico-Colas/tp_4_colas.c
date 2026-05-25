29f98b765df5352463664ed2e7a7042ac4e8295d
esteft660-ai <esteft660@gmail.com>
2026-05-25 16:45:20 -0600

    Update tp_4_colas.c

--- 04-trabajoPractico-Colas/tp_4_colas.c
+++ 04-trabajoPractico-Colas/tp_4_colas.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../libs/tipoElemento/headers/tipo_elemento.h"
+#include "../libs/validaciones/headers/Validacion_datos.h"
+#include "tp_4_colas.h"
+#include "../libs/colas/headers/colas.h"
+#include "../libs/validaciones/headers/validaciones.h"
+
+//Ejercicio 2
+bool c_ej2_existeclave(Cola c, int clave){
+
+}
+
+
+Cola c_ej2_colarelemento(Cola c, int posicionordinal, TipoElemento X){
+
+}
+
+
+Cola c_ej2_sacarelemento(Cola c, int clave){
+
+}
+
+
+int c_ej2_contarelementos(Cola c){
+
+}
+
+
+Cola c_ej2_copiar(Cola c){
+
+}
+
+Cola c_ej2_invertir(Cola c){
+}
+
+//Ejercicio 3 
+
+bool c_ej3_iguales(Cola c1, Cola c2){
+}
+
+//Ejercicio 4 
+
+Cola  c_ej4_colanorepetidos(Cola c){
+}
+
+//Ejercicio 5 
+
+cola c_ej5_divisortotal(Cola c){
+}
+
+//Ejercicio 6
+Lista c_ej6_comunesapilaycola(Pila p, Cola c){
+}
+
+//Ejercicio 7 
+Cola c_ej7_atenderclientes(Cola c1, Cola c2, Cola c3, int tiempoatencion){
+}
+
 
