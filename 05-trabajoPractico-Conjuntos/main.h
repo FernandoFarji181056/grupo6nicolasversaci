852d6b1826b9b0caa01b20633f99f6265f0b965b
esteft660-ai <esteft660@gmail.com>
2026-06-19 20:40:43 -0600

    Update main.h

--- 05-trabajoPractico-Conjuntos/main.h
+++ 05-trabajoPractico-Conjuntos/main.h
@@ -1,1 +1,14 @@
 
+#ifndef MAIN_TP5_H
+#define MAIN_TP5_H
+
+
+void ejecutar_TP5_EJ02();
+void ejecutar_TP5_EJ03();
+void ejecutar_TP5_EJ04();
+void ejecutar_TP5_EJ05();
+void ejecutar_TP5_EJ06();
+void ejecutar_TP5_EJ07();
+void ejecutar_TP5_EJ08();
+
+#endif
