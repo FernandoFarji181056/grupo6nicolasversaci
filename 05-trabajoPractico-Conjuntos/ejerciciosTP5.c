368910aefc9b3f59318291576822bcfb42c6c8e1
esteft660-ai <esteft660@gmail.com>
2026-06-19 20:49:31 -0600

    Update ejerciciosTP5.c

--- 05-trabajoPractico-Conjuntos/ejerciciosTP5.c
+++ 05-trabajoPractico-Conjuntos/ejerciciosTP5.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../libs/tipoElemento/headers/tipo_elemento.h"
+#include "../libs/validaciones/headers/Validacion_datos.h"
+#include "../libs/conjuntos/headers/conjuntos.h"
+#include "tp_5_conjuntos.h"
+#include "../libs/validaciones/headers/validaciones.h"
 
+
+void ejecutar_TP5_EJ02(){
+  
+}
+void ejecutar_TP5_EJ03(){
+  
+}
+void ejecutar_TP5_EJ04(){
+  
+}
+void ejecutar_TP5_EJ05(){
+  
+}
+void ejecutar_TP5_EJ06(){
+  
+}
+void ejecutar_TP5_EJ07(){
+  
+}
