40e8d501515e2ba4a2b430a3367a3a880bdf45cd
esteft660-ai <esteft660@gmail.com>
2026-06-28 19:24:04 -0600

    Create conjuntos_auxiliares.h

---
+++ 05-trabajoPractico-Conjuntos/conjuntos_auxiliares.h
@@ -0,0 +1,10 @@
+#ifndef CONJUNTOS_AUXILIARES_H
+#define CONJUNTOS_AUXILIARES_H
+
+#include <stdbool.h>
+#include "../libs/conjuntos/headers/conjuntos.h"
+
+
+bool cargarConjuntos(Conjunto A, Conjunto B);
+
+#endif
